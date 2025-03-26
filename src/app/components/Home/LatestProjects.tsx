96abb378e8cbd00aabc137d22d55e1ab2e0bc3e5
Lasha Bidzinashvili <116507932+Bidzinashvili@users.noreply.github.com>
2025-03-27 01:10:53 +0400

    Added a New Project

--- src/app/components/Home/LatestProjects.tsx
+++ src/app/components/Home/LatestProjects.tsx
@@ -7,8 +7,18 @@ export default function LatestProjects() {
             <p className="text-white font-medium pb-[30px]">Latest Projects</p>
 
             <div className="flex items-center gap-[30px] lg:items-start justify-center lg:justify-between pb-[100px] flex-col lg:flex-row">
-                <ProjectCard title="Sunnyside" description="Sunnyside is a vibrant agency website featuring a colorful design that highlights creativity and modern aesthetics." image="projects/project-1.png" url="https://sunnyside-weld.vercel.app/" />
-                <ProjectCard title="Sunnyside" description="Sunnyside is a vibrant agency website featuring a colorful design that highlights creativity and modern aesthetics." image="projects/project-1.png" url="https://sunnyside-weld.vercel.app/" />
+                <ProjectCard
+                    title="Sunnyside"
+                    description="Sunnyside is a vibrant agency website featuring a colorful design that highlights creativity and modern aesthetics."
+                    image="projects/project-1.png"
+                    url="https://sunnyside-weld.vercel.app/"
+                />
+                <ProjectCard
+                    title="MinimalBlog"
+                    description="MinimalBlog is a clean and modern blog website designed for sharing articles and insights with a minimalist aesthetic."
+                    image="projects/project-2.jpeg"
+                    url="https://minimal-blog-phi.vercel.app/"
+                />
             </div>
         </div>
     )
--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export default function ProjectCard({ title, description, image, url }: Props) {
     return (
-        <Link href={url} className='w-full  lg:max-w-[416px]'>
+        <Link href={url} className='w-full  lg:max-w-[416px]' target='_blank'>
             <Image className='rounded-t-[20px] w-full lg:w-auto' src={`/${image}`} alt='project picture' width={416} height={234} />
             <div className='bg-primary rounded-b-[20px] px-[20px] pt-[10px] pb-[25px]'>
                 <h2 className='text-white text-[16px] font-medium'>{title}</h2>
